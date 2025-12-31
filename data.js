635d77a42803c649177dfb8fcb0a9e96149098d9
Asimsanu <asimsanu@studioemfiz.com>
2025-12-31 13:02:00 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mb",
+      "name": "MB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 375,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.030747344370766427,
+          "pitch": 0.09438689198464978,
+          "rotation": 0,
+          "target": "1-wardrobe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wardrobe",
+      "name": "wardrobe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 375,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1388121578387675,
+          "pitch": 0.08389423548591779,
+          "rotation": 0,
+          "target": "0-mb"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
